71502327de78dc63265b926a5318ebb76493f746
GC7 <GrCOTE7@Gmail.com>
2019-09-04 22:24:16 +0200

    Nett storage

--- storage/cms/cache/aa/e5/default.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5d701723e1a9f965448875_ab42ca9232dbfbe8d3c07cf9cba3503aClass extends Cms\Classes\LayoutCode
-{
-
-}
--- storage/framework/classes.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'october\\demo\\Plugin' => 'plugins/october/demo/Plugin.php',
-  'backend\\Controllers\\index' => 'modules/backend/controllers/index.php',
-  'system\\Controllers\\settings' => 'modules/system/controllers/settings.php',
-  'system\\Controllers\\updates' => 'modules/system/controllers/updates.php',
-  'krisawzm\\demomanager\\Plugin' => 'plugins/krisawzm/demomanager/Plugin.php',
-  'Krisawzm\\DemoManager\\Classes\\DemoManager' => 'plugins/krisawzm/demomanager/classes/DemoManager.php',
-);
